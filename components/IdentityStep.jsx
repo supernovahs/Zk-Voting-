1169b476c81c2f887eb04d0db0a252a46c4ef549
supernovahs <harshitsinghal252@gmail.com>
2022-09-14 12:45:29 +0530

    Add Deterministic Identity Generation Option
    User signs secret string a private key and this hash is used to generate a new identity which is persisted as long as secret message and the same private key  is used

--- components/IdentityStep.jsx
+++ components/IdentityStep.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { Identity } from "@semaphore-protocol/identity";
 import { Button, Input } from "@chakra-ui/react";
-import { useEnsResolver } from "wagmi";
 import copy from "copy-to-clipboard";
 import { IconButton } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
+import { useSigner } from "wagmi";
 const { ethers } = require("ethers");
+
 export default function IdentityStep({}) {
+  const { data: signer, isError, isLoading } = useSigner();
   const [identity, setIdentity] = useState("");
   const [TrapdoorCopied, SetTrapdoorCopied] = useState(false);
   const [NullifierCopied, SetNullifierCopied] = useState(false);
   const [CommitmentCopied, SetCommitmentCopied] = useState(false);
-
+  const [SecretString, SetSecretString] = useState();
   async function checkidentity() {
     const identityval = window.localStorage.getItem("identitycommitment");
     console.log("identityval", identityval);
@@ -41,6 +43,16 @@ export default function IdentityStep({}) {
     window.localStorage.setItem("identitycommitment", identitynew);
   };
 
+  const CreateDeterministicidentity = async (hash) => {
+    const identitynew = new Identity(hash);
+    setIdentity(identitynew);
+    const publicid = identitynew.generateCommitment();
+    let a = ethers.BigNumber.from(publicid).toString();
+    console.log("identitycommitment", a);
+
+    window.localStorage.setItem("identitycommitment", identitynew);
+  };
+
   return (
     <div>
       <div>
@@ -61,8 +73,26 @@ export default function IdentityStep({}) {
               CreateNewidentity();
             }}
           >
-            Create Identity
+            Create Burner Identity
           </Button>
+
+          <div className="m-2 p-2 ">
+            <Input
+              placeholder="Enter a secret message to generate Identity "
+              value={SecretString}
+              onChange={(e) => SetSecretString(e.target.value)}
+            />
+            <Button
+              disabled={!SecretString}
+              onClick={async () => {
+                const hash = await signer.signMessage(SecretString);
+                CreateDeterministicidentity(hash);
+                SetSecretString("");
+              }}
+            >
+              Create a Deterministic Identity
+            </Button>
+          </div>
         </div>
         <div className="box-sizing border-2 border-black p-2 ">
           {identity && (
