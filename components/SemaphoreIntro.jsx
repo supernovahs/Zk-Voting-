ac3ff27e78e7cabf3ada98953d92bc336cf439a1
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-10-03 03:18:36 +0700

    fix: 🐛 home page hydration rendering error
    ✅ Closes: #2

--- components/SemaphoreIntro.jsx
+++ components/SemaphoreIntro.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React from 'react';
 
 export default function SemaphoreIntro() {
   return (
-    <div className="border-2 ">
-      <h2 className="text-5xl flex justify-center  mb-2 mt-4">
+    <div className='border-2 '>
+      <h2 className='text-5xl flex justify-center  mb-2 mt-4'>
         How does ZkVote work?
       </h2>
-      <div className="border-dashed border-black  box-content h-30 w-70 p-2 border-4 ">
-        <p className="italic ">
+      <div className='border-dashed border-black  box-content h-30 w-70 p-2 border-4 '>
+        <p className='italic '>
           Semaphore is a zero-knowledge protocol that allows users to prove
           their membership in a group and send signals such as votes or
           endorsements without revealing their identity. Additionally, it
           provides a simple mechanism to prevent double-signaling.
-          <li className="mb-2">Create or load an identity!</li>
-          <li className="mb-2">
+        </p>
+        <ul>
+          <li className='mb-2'>Create or load an identity!</li>
+          <li className='mb-2'>
             Create a new Vote Proposal or voting on an existing Proposal.
           </li>
-        </p>
+        </ul>
       </div>
     </div>
   );
