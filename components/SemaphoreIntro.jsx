d6f7dcc28a5909bdcd42dabeb5168afa63846291
Supernovahs.eth <91280922+supernovahs@users.noreply.github.com>
2022-10-05 15:54:35 +0530

    Update SemaphoreIntro.jsx

--- components/SemaphoreIntro.jsx
+++ components/SemaphoreIntro.jsx
@@ -20,31 +20,15 @@ export default function SemaphoreIntro() {
         membership in a group and send signals such as votes or endorsements
         without revealing their identity. Additionally, it provides a simple
         mechanism to prevent double-signaling.
+        <ul>
         <li className="mb-2">Create or load an identity!</li>
         <li className="mb-2">
           Create a new Vote Proposal or voting on an existing Proposal.
         </li>
+        </ul>
       </Text>
     </Box>
   );
 
-  return (
-    <div className="border-2 ">
-      <h2 className="text-5xl flex justify-center  mb-2 mt-4">
-        How does ZkVote work?
-      </h2>
-      <div className="border-dashed border-black  box-content h-30 w-70 p-2 border-4 ">
-        <p className="italic ">
-          Semaphore is a zero-knowledge protocol that allows users to prove
-          their membership in a group and send signals such as votes or
-          endorsements without revealing their identity. Additionally, it
-          provides a simple mechanism to prevent double-signaling.
-          <li className="mb-2">Create or load an identity!</li>
-          <li className="mb-2">
-            Create a new Vote Proposal or voting on an existing Proposal.
-          </li>
-        </p>
-      </div>
-    </div>
-  );
+ 
 }
